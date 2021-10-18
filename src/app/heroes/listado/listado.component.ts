49cfc3ae5909387a9c699af69adad89e8c0f1fc9
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-10-18 16:36:48 +0200

    3

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -2,13 +2,18 @@ import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
 
 
-@Component({
+@Component( {
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
-})
-export class ListadoComponent  {
+  styleUrls: [ './listado.component.css' ]
+} )
+export class ListadoComponent
+{
 
-  nombres: string [] = ['Superman','Electroman','Javaman','Bravoman'];
-  
+  nombres: string[] = [ 'Superman', 'Electroman', 'Javaman', 'Bravoman' ];
+
+
+  public items: string[] = [ 'ss', 'defe' ];
+
+
 }
